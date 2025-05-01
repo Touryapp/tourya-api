b12d79d3a5b24259fae19014a65f77eb531f85a9
eowkin <eowkin@gmail.com>
2025-05-01 11:13:17 -0400

    auhtntication_and_resgitration_user:
    1. Se agrego seguridad y creacion del token de autenticacion.
    2. Se agrego el servicio register.
    3. Se agrego el servicio authenticate.
    4. Se agrego el servicio activate-account.
    5. Se agrego el servicio authenticateWithGoogle.

---
+++ src/main/java/com/tourya/api/services/EmailService.java
@@ -0,0 +1,107 @@
+package com.tourya.api.services;
+
+import com.tourya.api.constans.enums.EmailTemplateNameEnum;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.springframework.mail.javamail.MimeMessageHelper.MULTIPART_MODE_MIXED;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender mailSender;
+    private final SpringTemplateEngine templateEngine;
+
+    @Async
+    public void sendEmail(
+            String to,
+            String username,
+            EmailTemplateNameEnum emailTemplate,
+            String confirmationUrl,
+            String activationCode,
+            String subject
+    ) throws MessagingException {
+        String templateName;
+        if (emailTemplate == null) {
+            templateName = "confirm-email";
+        } else {
+            templateName = emailTemplate.getName();
+        }
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(
+                mimeMessage,
+                MULTIPART_MODE_MIXED,
+                UTF_8.name()
+        );
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("username", username);
+        properties.put("confirmationUrl", confirmationUrl);
+        properties.put("activation_code", activationCode);
+
+        Context context = new Context();
+        context.setVariables(properties);
+
+        //helper.setFrom("contact@aliboucoding.com");
+        helper.setFrom("eowkin@usb.ve");
+        helper.setTo(to);
+        helper.setSubject(subject);
+
+        String template = templateEngine.process(templateName, context);
+
+        helper.setText(template, true);
+
+        mailSender.send(mimeMessage);
+    }
+
+    @Async
+    public void sendEmailTemporaryPassword(
+            String to,
+            String username,
+            EmailTemplateNameEnum emailTemplate,
+            String loginUrl,
+            String passwordTemporary,
+            String subject
+    ) throws MessagingException {
+        String templateName;
+        if (emailTemplate == null) {
+            templateName = "confirm-email";
+        } else {
+            templateName = emailTemplate.getName();
+        }
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(
+                mimeMessage,
+                MULTIPART_MODE_MIXED,
+                UTF_8.name()
+        );
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("username", username);
+        properties.put("loginUrl", loginUrl);
+        properties.put("password_temporary", passwordTemporary);
+
+        Context context = new Context();
+        context.setVariables(properties);
+
+        //helper.setFrom("contact@aliboucoding.com");
+        helper.setFrom("eowkin@usb.ve");
+        helper.setTo(to);
+        helper.setSubject(subject);
+
+        String template = templateEngine.process(templateName, context);
+
+        helper.setText(template, true);
+
+        mailSender.send(mimeMessage);
+    }
+}
