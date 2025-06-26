095efdc039aea1cd458bbe8f6f0457580959ee16
DanielOwkin <danielowkin@gmail.com>
2025-06-25 23:28:19 -0500

    DocumentRequest:
    Ajustes Services

--- src/main/java/com/tourya/api/services/RequestProviderGalleryService.java
+++ src/main/java/com/tourya/api/services/RequestProviderGalleryService.java
@@ -19,8 +19,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @Service
