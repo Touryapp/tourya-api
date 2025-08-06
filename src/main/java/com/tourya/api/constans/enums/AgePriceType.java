dc0fe46e0210cd5b53ffaff3d7ab7ea8b5f8090e
DanielOwkin <danielowkin@gmail.com>
2025-08-06 18:30:00 -0500

    :PopularTagTour
    Se modificaron las respuestas y se agrego el listado de AgePriceType

--- src/main/java/com/tourya/api/constans/enums/AgePriceType.java
+++ src/main/java/com/tourya/api/constans/enums/AgePriceType.java
@@ -1,7 +1,16 @@
 package com.tourya.api.constans.enums;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+@JsonFormat(shape = JsonFormat.Shape.OBJECT) // 🔹 Esto hace que se serialice como objeto
 public enum AgePriceType {
-    ADULT,
-    CHILD,
-    INFANT
+    ADULT("ADULT"),
+    CHILD("CHILD"),
+    INFANT("INFANT");
+
+    private final String name;
 }
--- src/main/java/com/tourya/api/services/TagCategoryService.java
+++ src/main/java/com/tourya/api/services/TagCategoryService.java
@@ -1,5 +1,6 @@
 package com.tourya.api.services;
 
+import com.tourya.api.models.responses.TagCategoryResponse;
 import com.tourya.api.repository.TagCategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,10 @@ public class TagCategoryService {
 
     private final TagCategoryRepository repository;
 
-    public List<String> getCategories() {
-        return repository.getAllCategories();
+    public List<TagCategoryResponse> getCategories() {
+        return repository.getAllCategories()
+                .stream()
+                .map(TagCategoryResponse::new) // convierte cada String a TagCategoryResponse
+                .toList();
     }
 }
