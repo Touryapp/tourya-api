006e12e94625da72197d52dbf5cb111e19c9b0e0
DanielOwkin <danielowkin@gmail.com>
2025-11-17 13:35:04 -0500

    ListReservation:
    Se creo la lista de reservaciones.

---
+++ src/main/java/com/tourya/api/models/responses/ReservationDetailsResponse.java
@@ -0,0 +1,57 @@
+package com.tourya.api.models.responses;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ReservationDetailsResponse {
+
+    // --- Reservation ---
+    private Long reservationId;
+    private String reservationDate;
+    private String reservationDeliveryStatus;
+    private String reservationCreatedDate;
+
+    // --- Payment ---
+    private Long paymentId;
+    private String paymentTransactionId;
+    private String payerName;
+    private String payerEmail;
+    private String payerPhone;
+    private String payerDocumentType;
+    private String payerDocumentNumber;
+
+    // --- Shopping Item ---
+    private Integer shoppingItemId;
+    private Double shoppingTotalPrice;
+    private Double shoppingUnitPrice;
+    private Integer shoppingQuantity;
+
+    private String productType;
+    private Integer productId;
+
+    private Long totalTourists; // BIGINT ✔
+
+    // --- Tour ---
+    private Integer tourId;
+    private String tourName;
+    private Integer tourCategoryId;
+    private Integer tourProviderId;
+
+    // --- Schedule ---
+    private Integer tourScheduleId;
+    private String scheduleDate;
+
+    private Integer slotId;
+    private String slotTimeStart;
+    private String slotTimeEnd;
+
+    private Integer minCapacity;
+    private Integer maxCapacity;
+
+    // --- Responsible ---
+    private String serviceResponsibleName;
+    private String serviceResponsibleEmail;
+    private String serviceResponsiblePhone;
+}
